fedfabeca5048dd4f55a70a5c586362cb96e4265
Thomas Marquart <thomas@marquart.se>
2026-03-12 14:57:38 +0100

    prepare new flat reductions, sofs, rc

--- flats/reduce_flats.sh
+++ flats/reduce_flats.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-parallel -j32 --eta '
-    sof={}
-    setting=$(basename "$sof" | sed "s/_[0-9].*//")
-    mkdir -p "$setting"
-    esorex --output-dir="$setting" cr2res_cal_flat "$sof" \
-        > "$setting/$(basename "$sof" .sof).log" 2>&1
-' ::: sof/*_flats.sof
+ls -d */flats.sof | sed 's|/flats.sof||' \
+| parallel -j16 --bar \
+    'cd {} && esorex --recipe-config=../../cr2res_cal_flat.rc cr2res_cal_flat flats.sof 2>&1 > esorex.log'
