da9915c641f99e0ba48a56e0019a80f29ffc6a25
Thomas Marquart <thomas@marquart.se>
2026-03-17 14:52:46 +0100

    use --with-editable instead

--- tellcorr.py
+++ tellcorr.py
@@ -97,7 +97,7 @@ def run_vipere(extracted_fits, workdir, setting, oset=None):
         oset = OSET_OVERRIDE.get(setting, f"1:{n_orders * 3 + 1}")
 
     cmd = [
-        'uv', 'run', '--with', str(Path.home() / 'vipere.git'), 'vipere',
+        'uv', 'run', '--with-editable', str(Path.home() / 'vipere.git'), 'vipere',
         str(extracted_fits),
         '-oset', oset,
         '-plot', '0',
